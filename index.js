04a9161ae8efa24745fe816d4abcb0b5064cc8ac
newvision11 <121769872+newvision11@users.noreply.github.com>
2023-07-12 16:47:59 +0100

    hnn

--- index.js
+++ index.js
@@ -2,28 +2,29 @@ var swiper = new Swiper('.swiper-container', {
     slidesPerView: 'auto',
     centeredSlides: true,
     pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+      el: '.swiper-pagination',
+      clickable: true,
     },
-});
-
-var colorButtons = document.querySelectorAll('.color-button');
-colorButtons.forEach(function (button) {
+  });
+  
+  var colorButtons = document.querySelectorAll('.color-button');
+  colorButtons.forEach(function (button) {
     button.addEventListener('click', function () {
-        var color = this.dataset.color;
-        var slideIndex = getSlideIndexByColor(color);
-        if (slideIndex !== -1) {
-            swiper.slideTo(slideIndex);
-        }
+      var color = this.dataset.color;
+      var slideIndex = getSlideIndexByColor(color);
+      if (slideIndex !== -1) {
+        swiper.slideTo(slideIndex, 0); // Set speed to 0 for instant transition
+      }
     });
-});
-
-function getSlideIndexByColor(color) {
+  });
+  
+  function getSlideIndexByColor(color) {
     var slides = document.querySelectorAll('.swiper-slide');
     for (var i = 0; i < slides.length; i++) {
-        if (slides[i].dataset.color === color) {
-            return i;
-        }
+      if (slides[i].dataset.color === color) {
+        return i;
+      }
     }
     return -1;
-}
+  }
+  
